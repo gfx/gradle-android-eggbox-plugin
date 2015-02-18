4ed40c1575e05063df4ae95fb2fd8bb882c0594a
FUJI Goro (gfx) <gfuji@cpan.org>
2015-02-18 23:06:53 +0900

    WIP

--- plugin/src/main/groovy/com/cookpad/gradle/eggbox/EggboxPlugin.groovy
+++ plugin/src/main/groovy/com/cookpad/gradle/eggbox/EggboxPlugin.groovy
@@ -29,49 +29,111 @@ public class EggboxPlugin implements Plugin<Project> {
     void apply(final Project project) {
         project.extensions.add(EggboxExtension.NAME, EggboxExtension as Object)
 
+        final lockFile = project.file('eggbox.xml')
+        final deployDir = project.file('.eggbox')
+
         final android = project.extensions.findByType(AppExtension)
         if (!android) {
             throw new GradleException("No android extension found in ${project}")
+        }
+
+        if (lockFile.exists()) {
+            project.logger.info("[eggbox] use locked dependencies in ${lockFile}")
+
+            def deps = new XmlParser().parse(lockFile)
+
+            deps.each { Node artifact ->
+                def buildType = artifact.attribute('build-type').toString()
+                def flavor = artifact.attribute('flavor')
+
+                def decl = flavor ? "${flavor}${buildType.capitalize()}Compile" : "${buildType}Compile"
+
+                artifact.each { Node item ->
+                    if (item.name() == "compile") {
+                        def value = item.text().trim()
+                        println("${decl}('${value}')")
+                        project.dependencies.invokeMethod(decl, value)
+                    } else if (item.name() == "test") {
+
+                    } else {
+                        project.logger.error("Unexpected node: $item")
+                    }
+
+                }
+            }
+        } else {
+            project.logger.info("[eggbox] lock dependencies")
+
+            project.afterEvaluate {
+                final xml = new XmlParser().parseText('<dependency />')
+
+                android.applicationVariants.all { ApplicationVariant variant ->
+                    def buildType = variant.buildType.name
+                    def flavor = variant.flavorName
+
+                    def attributes = new HashMap<String, String>()
+                    if (buildType) {
+                        attributes['build-type'] = buildType
+                    }
+                    if (flavor) {
+                        attributes['flavor'] = flavor
+                    }
+
+                    def buildNode = xml.appendNode('artifact', attributes)
+                    variant.compileLibraries.each { File file ->
+                        if (file.toString().contains('/.gradle/caches/')) {
+                            buildNode.appendNode('compile', artifactId(file))
+                        }
+                    }
+
+                    def testVariant = variant.testVariant
+                    if (testVariant) {
+                        def testNode = buildNode.appendNode('test')
+                        testVariant.compileLibraries.each { File file ->
+                            if (file.toString().contains('/.gradle/caches/')) {
+                                testNode.appendNode('compile', artifactId(file))
+                            }
+                        }
+                    }
+                }
+
+                final writer = new FileWriter(lockFile)
+                final xmlNodePrinter = new XmlNodePrinter(new PrintWriter(writer))
+                xmlNodePrinter.print(xml)
+                writer.close()
+            }
         }
 
         project.repositories.all { DefaultMavenArtifactRepository a ->
             println("repository: ${a.name} ${a.url}")
         }
 
-        project.afterEvaluate {
-            final xml = new XmlParser().parseText('<dependency />')
+    }
 
-            android.applicationVariants.all { ApplicationVariant variant ->
-                def buildType = variant.buildType.name
-                def flavor = variant.flavorName
+    static class ArtifactId {
+        String groupName
+        String artifactName
+        String version
 
-                def attributes = new HashMap<String, String>()
-                if (buildType) {
-                    attributes['build-type'] = buildType
-                }
-                if (flavor) {
-                    attributes['flavor'] = flavor
-                }
-
-                def artifact = { File file ->
-                    // file is like $cache/io.reactivex/rxjava/1.0.4/a607b0e12d2de769dc219e00b60a9b3d9e730d/rxjava-1.0.4.jar
-                    def version = file.parentFile.parentFile.name
-                    def artifactId = file.parentFile.parentFile.parentFile.name
-                    def groupId = file.parentFile.parentFile.parentFile.parentFile.name
-                    return "${groupId}:${artifactId}:${version}"
-                }
-
-                def compileNode = xml.appendNode('compile', attributes)
-                variant.compileLibraries.each { File file ->
-                    compileNode.appendNode('artifact', artifact(file))
-                }
-            }
-
-
-            final writer = new FileWriter("${project.rootDir}/eggbox.xml")
-            final xmlNodePrinter = new XmlNodePrinter(new PrintWriter(writer))
-            xmlNodePrinter.print(xml)
-            writer.close()
+        public ArtifactId(String groupId, String artifactName, String version) {
+            this.groupName = groupId
+            this.artifactName = artifactName
+            this.version = version
         }
+
+        @Override
+        public String toString() {
+            return "$groupName:$artifactName:$version"
+        }
+    }
+
+    static ArtifactId artifactId(File file) {
+        println("file: $file")
+        // file is something like $cache/io.reactivex/rxjava/1.0.4/a607b0e12d2de769dc219e00b60a9b3d9e730d/rxjava-1.0.4.jar
+        def version = file.parentFile.parentFile.name
+        def artifactName = file.parentFile.parentFile.parentFile.name
+        def groupName = file.parentFile.parentFile.parentFile.parentFile.name
+
+        return new ArtifactId(groupName, artifactName, version)
     }
 }
